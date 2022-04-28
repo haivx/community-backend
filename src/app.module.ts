3c2edf0657cb0f9f26fc2bad5343833983de8c51
Xuan Hai <haivx@vmodev.com>
2022-04-28 15:22:54 +0700

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto {
+    name: string;
+    age: number;
+    email: string;
+    phone: string;
+    address: string;
+}
