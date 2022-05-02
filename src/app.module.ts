0dc60105be28fafecb9e67ff498d59a21e68422b
Xuan Hai <haivx@vmodev.com>
2022-05-02 17:34:33 +0700

    add validate schema for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from "@nestjs/typeorm"
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TasksModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: 'localhost',
-      port: 5432,
-      username: "postgres",
-      password: "postgres",
-      database: "task-management",
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
-    AuthModule
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: 'postgres',
+          password: 'postgres',
+          database: 'task-management',
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    AuthModule,
   ],
 })
 export class AppModule {}
